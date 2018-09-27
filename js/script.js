9f5607e9f53edbd0150a7b129eded4ee30eed601
dimamuha637296 <41823536+dimamuha637296@users.noreply.github.com>
2018-09-27 05:19:37 +0300

    Добавлен слайдер фоновых изображений в хэдэре

--- js/script.js
+++ js/script.js
@@ -2,7 +2,13 @@ $(document).ready(function () {
     $('.gallery').owlCarousel({
         dots: false,
         loop: true,
-        items:5.5,
+        items: 5.5,
         center: false
+    })
+
+    $('.mainSlider').owlCarousel({
+        items: 1,
+        loop: true,
+        dotsContainer: '#customDots'
     })
 });
